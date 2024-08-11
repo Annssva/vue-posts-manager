ae7c583e75ec2c288f2903e3f87c4a24635fbc99
annssva <sysoevaannaa@mail.ru>
2024-08-11 13:54:11 +0300

    added dynamic pagination

--- src/App.vue
+++ src/App.vue
@@ -29,19 +29,22 @@
         v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
-    <div class="page__wrapper">
-      <div
-          v-for="pageNumber in totalPages"
-          :key="pageNumber"
-          class="page"
-          :class="{
-            'current-page': page === pageNumber
-          }"
-          @click="changePage(pageNumber)"
-      >
-        {{pageNumber}}
-      </div>
+    <div ref="observer" class="observer">
+
     </div>
+<!--    <div class="page__wrapper">-->
+<!--      <div-->
+<!--          v-for="pageNumber in totalPages"-->
+<!--          :key="pageNumber"-->
+<!--          class="page"-->
+<!--          :class="{-->
+<!--            'current-page': page === pageNumber-->
+<!--          }"-->
+<!--          @click="changePage(pageNumber)"-->
+<!--      >-->
+<!--        {{pageNumber}}-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
@@ -95,10 +98,10 @@ export default {
     showModal() {
       this.isModalOpen = true;
     },
-    changePage(pageNumber) {
-      this.page = pageNumber;
-      // this.fetchPosts()
-    },
+    // changePage(pageNumber) {
+    //   this.page = pageNumber;
+    //   // this.fetchPosts() // // это слишком изи, внизу с вотч сделали
+    // },
     async fetchPosts() {
       try {
         this.isPostsLoading = true
@@ -115,10 +118,37 @@ export default {
       } finally {
         this.isPostsLoading = false
       }
+    },
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert('Ошибка!')
+      }
     }
   },
   mounted() {
-    this.fetchPosts()
+    this.fetchPosts();
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0,
+    }
+    const callback = (entries, observer) => {
+      if(entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts()
+      }
+    }
+
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
@@ -136,9 +166,9 @@ export default {
     //     })
     //   }
 
-      page() {
-        this.fetchPosts()
-      }
+      // page() {
+      //   this.fetchPosts()
+      // }
     }
 }
 </script>
@@ -175,6 +205,11 @@ export default {
 .current-page {
   border: 2px solid teal;
   padding: 9px;
+}
+
+.observer {
+  height: 30px;
+  background: green;
 }
 
 </style>
