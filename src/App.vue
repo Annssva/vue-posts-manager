8aff1c3d7590d8f122642478adfdf24ff4aa0877
annssva <sysoevaannaa@mail.ru>
2024-08-11 13:36:19 +0300

    added pages

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,19 @@
         v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
+    <div class="page__wrapper">
+      <div
+          v-for="pageNumber in totalPages"
+          :key="pageNumber"
+          class="page"
+          :class="{
+            'current-page': page === pageNumber
+          }"
+          @click="changePage(pageNumber)"
+      >
+        {{pageNumber}}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -56,6 +69,9 @@ export default {
       isPostsLoading: false,
       selectedSort: '',
       searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         {
           value: 'title',
@@ -79,10 +95,20 @@ export default {
     showModal() {
       this.isModalOpen = true;
     },
+    changePage(pageNumber) {
+      this.page = pageNumber;
+      // this.fetchPosts()
+    },
     async fetchPosts() {
       try {
         this.isPostsLoading = true
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
         this.posts = response.data;
       } catch (e) {
         alert('Ошибка!')
@@ -102,14 +128,18 @@ export default {
     sortedAndSearchedPosts() {
       return this.sortedPosts.filter( post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
     }
-  }
-    // watch: {
+  },
+    watch: {
     //   selectedSort(newValue) {
     //     this.posts.sort((post1, post2) => {
     //       return post1[newValue]?.localeCompare(post2[newValue])
     //     })
     //   }
-    // }
+
+      page() {
+        this.fetchPosts()
+      }
+    }
 }
 </script>
 
@@ -124,10 +154,27 @@ export default {
   padding: 10px;
 }
 
-
 .app__buttons {
   margin: 15px;
   display: flex;
   justify-content: space-between;
 }
+
+.page__wrapper {
+  display: flex;
+  flex-direction: row;
+  margin-top: 15px;
+  gap: 5px
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.current-page {
+  border: 2px solid teal;
+  padding: 9px;
+}
+
 </style>
