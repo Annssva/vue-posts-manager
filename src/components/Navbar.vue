8b771879210df903548f713163a49f3bde4b25bb
annssva <sysoevaannaa@mail.ru>
2024-08-12 11:39:12 +0300

    fixed about and navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar">
-    <h3 @click="$router.push('/')">Vue 3 project</h3>
+    <h3 @click="$router.push('/')">Vue Posts Manager</h3>
     <div class="navbar__buttons">
       <button-ui @click="$router.push('/about')">О сайте</button-ui>
       <button-ui @click="$router.push('/posts')">Посты</button-ui>
